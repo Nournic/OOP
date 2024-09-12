6b21e54fa9bd8f17029d737e870902bc6febccd4
Плохин <ploxind@bk.ru>
2024-09-12 17:42:23 +0400

    Task 4

--- LR/src/main/java/ru/ssau/tk/nour/oop/functions/TabulatedFunction.java
+++ LR/src/main/java/ru/ssau/tk/nour/oop/functions/TabulatedFunction.java
@@ -6,13 +6,15 @@ public class TabulatedFunction {
 
     public TabulatedFunction(double leftX, double rightX, int pointsCount) {
         functionPoints = new FunctionPoint[pointsCount];
+        for (int i = 0; i < functionPoints.length; i++)
+            functionPoints[i] = new FunctionPoint();
 
         this.pointsCount = pointsCount;
         double step = (rightX - leftX) / (pointsCount - 1);
 
         int i;
         double current_step;
-        for(i = 0, current_step=leftX; i < pointsCount; i++) {
+        for (i = 0, current_step = leftX; i < pointsCount; i++) {
             functionPoints[i].setX(current_step);
             current_step += step;
         }
@@ -20,15 +22,47 @@ public class TabulatedFunction {
 
     public TabulatedFunction(double leftX, double rightX, double[] values) {
         functionPoints = new FunctionPoint[values.length];
-        double step = (rightX - leftX) / values.length;
+        for (int i = 0; i < functionPoints.length; i++)
+            functionPoints[i] = new FunctionPoint();
+
+        double step = (rightX - leftX) / (values.length - 1);
         pointsCount = values.length;
 
         int i;
         double current_step;
-        for(i = 0, current_step=leftX; i < values.length; i++) {
+        for (i = 0, current_step = leftX; i < values.length; i++) {
             functionPoints[i].setX(current_step);
             functionPoints[i].setY(values[i]);
-            current_step+=step;
+            current_step += step;
         }
+    }
+
+    public double getLeftDomainBorder() {
+        int left_index = 0;
+        return functionPoints[left_index].getX();
+    }
+
+    public double getRightDomainBorder() {
+        int right_index = pointsCount - 1;
+        return functionPoints[right_index].getX();
+    }
+
+    public double getFunctionValue(double x) {
+        double answer = Double.NaN;
+
+        if(x>=getLeftDomainBorder() && x<=getRightDomainBorder()) {
+            FunctionPoint next_point = functionPoints[1];
+            FunctionPoint prev_point = functionPoints[0];
+            for (int i = 0; i < pointsCount; i++){
+                if (functionPoints[i].getX() >= x && x != getLeftDomainBorder()) {
+                    next_point = functionPoints[i];
+                    prev_point = functionPoints[i-1];
+                    break;
+                }
+            }
+            answer = ((x - prev_point.getX())/(next_point.getX() - prev_point.getX())
+                    * (next_point.getY() - prev_point.getY()) + prev_point.getY());
+        }
+        return answer;
     }
 }
--- LR/src/test/java/ru/ssau/tk/nour/oop/functions/TabulatedFunctionTest.java
+++ LR/src/test/java/ru/ssau/tk/nour/oop/functions/TabulatedFunctionTest.java
@@ -1,5 +1,57 @@
 package ru.ssau.tk.nour.oop.functions;
 
-class TabulatedFunctionTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+class TabulatedFunctionTest {
+    TabulatedFunction tabulatedFunctionArray;
+    TabulatedFunction tabulatedFunctionPointsCount;
+
+    @BeforeEach
+    void init(){
+        double[] values = {1,2,4,8,16,32,64,128,256};
+        tabulatedFunctionArray = new TabulatedFunction(-5.0, 5.0, values);
+        tabulatedFunctionPointsCount = new TabulatedFunction(-5.0, 5.0, 9);
+    }
+    @Test
+    void getLeftDomainBorder() {
+        double expected = -5.0;
+
+        Assertions.assertEquals(expected, tabulatedFunctionArray.getLeftDomainBorder());
+        Assertions.assertEquals(expected, tabulatedFunctionPointsCount.getLeftDomainBorder());
+    }
+
+    @Test
+    void getRightDomainBorder() {
+        double expected = 5.0;
+
+        Assertions.assertEquals(expected, tabulatedFunctionArray.getRightDomainBorder());
+        Assertions.assertEquals(expected, tabulatedFunctionPointsCount.getRightDomainBorder());
+    }
+
+    @Test
+    void getFunctionValueArray() {
+        double[] expected = {1,2,4,8,16,32,64,128,256};
+
+        double step = 10.0 / 8.0;
+        double current_step = -5.0;
+        for (double v : expected) {
+            Assertions.assertEquals(v, tabulatedFunctionArray.getFunctionValue(current_step));
+            current_step += step;
+        }
+
+        Assertions.assertEquals(Double.NaN, tabulatedFunctionArray.getFunctionValue(-999));
+        Assertions.assertEquals(Double.NaN, tabulatedFunctionArray.getFunctionValue(999));
+    }
+
+    @Test
+    void getFunctionValuePointsCount() {
+        double expected = 0;
+
+        Assertions.assertEquals(expected, tabulatedFunctionPointsCount.getFunctionValue(0));
+        Assertions.assertEquals(Double.NaN, tabulatedFunctionPointsCount.getFunctionValue(-999));
+        Assertions.assertEquals(Double.NaN, tabulatedFunctionPointsCount.getFunctionValue(999));
+    }
 }
