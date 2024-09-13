73f38744a8e614106ee7fa86177f57b196fc7c98
Плохин <ploxind@bk.ru>
2024-09-13 06:29:34 +0400

    Task 4.5

--- LR/src/main/java/ru/ssau/tk/nour/oop/functions/LinkedListTabulatedFunction.java
+++ LR/src/main/java/ru/ssau/tk/nour/oop/functions/LinkedListTabulatedFunction.java
@@ -6,17 +6,22 @@ public class LinkedListTabulatedFunction {
     private int last_search_index;
     private int pointsCount;
 
-    private FunctionNode getNodeByIndex(int index){
+    private void checkIndex(int index) throws FunctionPointIndexOutOfBoundsException {
+        if (index > pointsCount - 1 || index < 0)
+            throw new FunctionPointIndexOutOfBoundsException();
+    }
+
+    private FunctionNode getNodeByIndex(int index) {
         FunctionNode current_node = head.getNext();
         int cur_index = 0;
 
-        if(last_search_index - index < index) {
+        if (last_search_index - index < index) {
             current_node = last_search;
             cur_index = last_search_index;
         }
 
-        while(cur_index++ != index)
-            current_node=current_node.getNext();
+        while (cur_index++ != index)
+            current_node = current_node.getNext();
 
         last_search = current_node;
         last_search_index = cur_index;
@@ -24,15 +29,15 @@ public class LinkedListTabulatedFunction {
         return current_node;
     }
 
-    private FunctionNode addNodeToTail(){
-        FunctionNode new_node = new FunctionNode(head.getPrev(),head);
+    private FunctionNode addNodeToTail() {
+        FunctionNode new_node = new FunctionNode(head.getPrev(), head);
         head.getPrev().setNext(new_node);
         head.setPrev(new_node);
 
         return new_node;
     }
 
-    private FunctionNode addNodeByIndex(int index){
+    private FunctionNode addNodeByIndex(int index) {
         FunctionNode current_node = getNodeByIndex(index);
         FunctionNode new_node = new FunctionNode(current_node.getPrev(), current_node.getNext());
 
@@ -42,11 +47,145 @@ public class LinkedListTabulatedFunction {
         return new_node;
     }
 
-    private FunctionNode deleteNodeByIndex(int index){
+    private FunctionNode deleteNodeByIndex(int index) {
         FunctionNode current_node = getNodeByIndex(index);
         current_node.getPrev().setNext(current_node.getNext());
         current_node.getNext().setPrev(current_node.getPrev());
 
         return current_node;
+    }
+
+    public LinkedListTabulatedFunction(double leftX, double rightX, int pointsCount) throws IllegalArgumentException {
+        if (leftX >= rightX || pointsCount < 2)
+            throw new IllegalArgumentException();
+
+        head = new FunctionNode(null, null);
+        head.setNext(head);
+        head.setPrev(head);
+
+        this.pointsCount = pointsCount;
+        double step = (rightX - leftX) / (pointsCount - 1);
+
+        int i;
+        double current_step;
+        for (i = 0, current_step = leftX; i < pointsCount; i++) {
+            addNodeToTail().setData(new FunctionPoint(current_step, 0));
+            current_step += step;
+        }
+    }
+
+    public LinkedListTabulatedFunction(double leftX, double rightX, double[] values) throws IllegalArgumentException {
+        if (leftX >= rightX || values.length < 2)
+            throw new IllegalArgumentException();
+
+        head = new FunctionNode(null, null);
+        head.setNext(head);
+        head.setPrev(head);
+
+        this.pointsCount = values.length;
+        double step = (rightX - leftX) / (pointsCount - 1);
+
+        int i;
+        double current_step;
+        for (i = 0, current_step = leftX; i < pointsCount; i++) {
+            addNodeToTail().setData(new FunctionPoint(current_step, values[i]));
+            current_step += step;
+        }
+    }
+
+    public double getLeftDomainBorder() {
+        return head.getNext().getData().getX();
+    }
+
+    public double getRightDomainBorder() {
+        return head.getPrev().getData().getX();
+    }
+
+    public double getFunctionValue(double x) {
+        double answer = Double.NaN;
+
+        if (x >= getLeftDomainBorder() && x <= getRightDomainBorder()) {
+            FunctionPoint next_point = getPoint(1);
+            FunctionPoint prev_point = getPoint(0);
+            for (int i = 0; i < pointsCount; i++) {
+                if (getPoint(i).getX() >= x && x != getLeftDomainBorder()) {
+                    next_point = getPoint(i);
+                    prev_point = getPoint(i - 1);
+                    break;
+                }
+            }
+            answer = ((x - prev_point.getX()) / (next_point.getX() - prev_point.getX())
+                    * (next_point.getY() - prev_point.getY()) + prev_point.getY());
+        }
+        return answer;
+    }
+
+    public int getPointsCount() {
+        return pointsCount;
+    }
+
+    public FunctionPoint getPoint(int index) throws FunctionPointIndexOutOfBoundsException {
+        checkIndex(index);
+        return getNodeByIndex(index).getData();
+    }
+
+    public void setPoint(int index, FunctionPoint point) throws FunctionPointIndexOutOfBoundsException, InappropriateFunctionPointException {
+        checkIndex(index);
+
+        // Точка за левой границей
+        if (point.getX() < head.getNext().getData().getX())
+            throw new InappropriateFunctionPointException();
+
+        // Точка за правой границей
+        if (point.getX() > head.getPrev().getData().getX())
+            throw new InappropriateFunctionPointException();
+
+        getNodeByIndex(index).setData(point);
+    }
+
+    public double getPointX(int index) throws FunctionPointIndexOutOfBoundsException {
+        checkIndex(index);
+        return getNodeByIndex(index).getData().getX();
+    }
+
+    public void setPointX(int index, double x) throws FunctionPointIndexOutOfBoundsException {
+        checkIndex(index);
+        getNodeByIndex(index).getData().setX(x);
+    }
+
+    public double getPointY(int index) throws FunctionPointIndexOutOfBoundsException {
+        checkIndex(index);
+        return getNodeByIndex(index).getData().getY();
+    }
+
+    public void setPointY(int index, double y) throws FunctionPointIndexOutOfBoundsException {
+        checkIndex(index);
+        getNodeByIndex(index).getData().setY(y);
+    }
+
+    public void deletePoint(int index) throws FunctionPointIndexOutOfBoundsException, IllegalStateException {
+        checkIndex(index);
+
+        if (pointsCount < 3)
+            throw new IllegalStateException();
+
+        deleteNodeByIndex(index);
+        pointsCount--;
+    }
+
+    public void addPoint(FunctionPoint point) throws InappropriateFunctionPointException {
+        int index_insert = 0;
+        for (int i = 0; i < pointsCount; i++) {
+            if (getNodeByIndex(i).getData().getX() >= point.getX()) {
+                if (getNodeByIndex(i).getData().getX() == point.getX())
+                    throw new InappropriateFunctionPointException();
+
+                index_insert = i;
+                break;
+            }
+        }
+
+        addNodeByIndex(index_insert).setData(point);
+        pointsCount++;
     }
 }
---
+++ LR/src/test/java/ru/ssau/tk/nour/oop/functions/LinkedListTabulatedFunctionTest.java
@@ -0,0 +1,260 @@
+package ru.ssau.tk.nour.oop.functions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkedListTabulatedFunctionTest {
+    LinkedListTabulatedFunction linkedListTabulatedFunction;
+    LinkedListTabulatedFunction linkedListTabulatedFunctionPointsCount;
+
+    @BeforeEach
+    void init() {
+        double[] values = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+        linkedListTabulatedFunction = new LinkedListTabulatedFunction(-5.0, 5.0, values);
+        linkedListTabulatedFunctionPointsCount = new LinkedListTabulatedFunction(-5.0, 5.0, 9);
+    }
+
+    @Test
+    void getLeftDomainBorder() {
+        double expected = -5.0;
+
+        assertEquals(expected, linkedListTabulatedFunction.getLeftDomainBorder());
+        assertEquals(expected, linkedListTabulatedFunctionPointsCount.getLeftDomainBorder());
+    }
+
+    @Test
+    void getRightDomainBorder() {
+        double expected = 5.0;
+
+        Assertions.assertEquals(expected, linkedListTabulatedFunction.getRightDomainBorder());
+        Assertions.assertEquals(expected, linkedListTabulatedFunctionPointsCount.getRightDomainBorder());
+    }
+
+    @Test
+    void getFunctionValueArray() {
+        double[] expected = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+
+        double step = 10.0 / 8.0;
+        double current_step = -5.0;
+        for (double v : expected) {
+            Assertions.assertEquals(v, linkedListTabulatedFunction.getFunctionValue(current_step));
+            current_step += step;
+        }
+
+        Assertions.assertEquals(Double.NaN, linkedListTabulatedFunction.getFunctionValue(-999));
+        Assertions.assertEquals(Double.NaN, linkedListTabulatedFunction.getFunctionValue(999));
+    }
+
+    @Test
+    void getFunctionValuePointsCount() {
+        double expected = 0;
+
+        Assertions.assertEquals(expected, linkedListTabulatedFunctionPointsCount.getFunctionValue(0));
+        Assertions.assertEquals(Double.NaN, linkedListTabulatedFunctionPointsCount.getFunctionValue(-999));
+        Assertions.assertEquals(Double.NaN, linkedListTabulatedFunctionPointsCount.getFunctionValue(999));
+    }
+
+    @Test
+    void getPointsCount() {
+        int expected = 9;
+
+        Assertions.assertEquals(expected, linkedListTabulatedFunction.getPointsCount());
+        Assertions.assertEquals(expected, linkedListTabulatedFunctionPointsCount.getPointsCount());
+    }
+
+    @Test
+    void getPointArray() {
+        double[] expected = {1, 2, 4, 8, 16, 32, 64, 128, 256};
+
+        for (int i = 0; i < expected.length; i++) {
+            Assertions.assertEquals(expected[i], linkedListTabulatedFunction.getPointY(i));
+        }
+    }
+
+    @Test
+    void getPointPointsCount() {
+        double expect1 = -5;
+        double expect2 = 5;
+
+        Assertions.assertEquals(expect1, linkedListTabulatedFunctionPointsCount.getPoint(0).getX());
+        Assertions.assertEquals(expect2, linkedListTabulatedFunctionPointsCount.getPoint(
+                linkedListTabulatedFunctionPointsCount.getPointsCount() - 1).getX());
+    }
+
+    @Test
+    void setPointInBorder() {
+        double expect = -4;
+
+        try {
+            FunctionPoint functionPoint = Mockito.mock(FunctionPoint.class);
+            Mockito.when(functionPoint.getX()).thenReturn(expect);
+            linkedListTabulatedFunction.setPoint(0, functionPoint);
+
+            Assertions.assertEquals(expect, linkedListTabulatedFunction.getPointX(0));
+        } catch (InappropriateFunctionPointException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void getPointX() {
+        double expect1 = -5;
+        double expect2 = 5;
+
+        Assertions.assertEquals(expect1, linkedListTabulatedFunctionPointsCount.getPointX(0));
+        Assertions.assertEquals(expect2, linkedListTabulatedFunctionPointsCount.getPointX(
+                linkedListTabulatedFunctionPointsCount.getPointsCount() - 1));
+    }
+
+    @Test
+    void getPointY() {
+        double expect1 = 1;
+        double expect2 = 256;
+
+        Assertions.assertEquals(expect1, linkedListTabulatedFunction.getPointY(0));
+        Assertions.assertEquals(expect2, linkedListTabulatedFunction.getPointY(
+                linkedListTabulatedFunctionPointsCount.getPointsCount() - 1));
+    }
+
+    @Test
+    void deletePoint() {
+        double expect = linkedListTabulatedFunction.getPoint(1).getX();
+        double expect_points = linkedListTabulatedFunction.getPointsCount() - 1;
+
+        linkedListTabulatedFunction.deletePoint(0);
+
+        Assertions.assertEquals(expect, linkedListTabulatedFunction.getPoint(0).getX());
+        Assertions.assertEquals(expect_points, linkedListTabulatedFunction.getPointsCount());
+    }
+
+    @Test
+    void addPoint1() {
+        FunctionPoint functionPoint = new FunctionPoint(-5.5,0);
+        int expect_points = linkedListTabulatedFunction.getPointsCount() + 1;
+
+        try {
+            linkedListTabulatedFunction.addPoint(functionPoint);
+            Assertions.assertSame(functionPoint, linkedListTabulatedFunction.getPoint(0));
+            Assertions.assertEquals(expect_points, linkedListTabulatedFunction.getPointsCount());
+        } catch (InappropriateFunctionPointException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void addPoint2() {
+        FunctionPoint functionPoint = new FunctionPoint(5.5,0);
+        int expect_points = linkedListTabulatedFunction.getPointsCount() + 1;
+
+        try {
+            linkedListTabulatedFunction.addPoint(functionPoint);
+            Assertions.assertSame(functionPoint, linkedListTabulatedFunction.getPoint(
+                    linkedListTabulatedFunction.getPointsCount() - 1));
+            Assertions.assertEquals(expect_points, linkedListTabulatedFunction.getPointsCount());
+        } catch (InappropriateFunctionPointException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void addPoint3() {
+        FunctionPoint functionPoint = new FunctionPoint(-4.5,0);
+        int expect_points = linkedListTabulatedFunction.getPointsCount() + 1;
+
+        try {
+            linkedListTabulatedFunction.addPoint(functionPoint);
+            Assertions.assertSame(functionPoint, linkedListTabulatedFunction.getPoint(1));
+            Assertions.assertEquals(expect_points, linkedListTabulatedFunction.getPointsCount());
+        } catch (InappropriateFunctionPointException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void addPoint4() {
+        FunctionPoint functionPoint = new FunctionPoint(4.5,0);
+        int expect_points = linkedListTabulatedFunction.getPointsCount() + 1;
+
+        try {
+            linkedListTabulatedFunction.addPoint(functionPoint);
+            Assertions.assertSame(functionPoint, linkedListTabulatedFunction.getPoint(
+                    linkedListTabulatedFunction.getPointsCount() - 2
+            ));
+            Assertions.assertEquals(expect_points, linkedListTabulatedFunction.getPointsCount());
+        } catch (InappropriateFunctionPointException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void createFunctionWithLesserThan2Points() {
+        Assertions.assertThrowsExactly(IllegalArgumentException.class,
+                () -> new TabulatedFunction(-5, 5, 1));
+        Assertions.assertThrowsExactly(IllegalArgumentException.class,
+                () -> new TabulatedFunction(-5, 5, -1));
+    }
+
+    @Test
+    void createFunctionWithEqualsX() {
+        Assertions.assertThrowsExactly(IllegalArgumentException.class,
+                () -> new TabulatedFunction(0, 0, 10));
+    }
+
+    @Test
+    void createFunctionWithRightXGreaterThanLeftX() {
+        Assertions.assertThrowsExactly(IllegalArgumentException.class,
+                () -> new TabulatedFunction(5, -5, 10));
+    }
+    @Test
+    void setPointExceptions() {
+        FunctionPoint functionPoint = new FunctionPoint(-5.01, 0);
+
+        Assertions.assertThrowsExactly(InappropriateFunctionPointException.class,
+                () -> linkedListTabulatedFunction.setPoint(0, functionPoint));
+
+        functionPoint.setX(5.01);
+
+        Assertions.assertThrowsExactly(InappropriateFunctionPointException.class,
+                () -> linkedListTabulatedFunction.setPoint(0, functionPoint));
+    }
+    @Test
+    void setPoint() {
+        FunctionPoint functionPoint = new FunctionPoint(1, 5);
+        try {
+            Assertions.assertNotEquals(functionPoint, linkedListTabulatedFunction.getPoint(5));
+            linkedListTabulatedFunction.setPoint(5, functionPoint);
+            Assertions.assertEquals(functionPoint, linkedListTabulatedFunction.getPoint(5));
+        } catch (InappropriateFunctionPointException e) {
+            Assertions.assertEquals(-1, 1);
+        }
+    }
+    @Test
+    void deletePointTwoPoints() {
+        TabulatedFunction linkedListTabulatedFunction = new TabulatedFunction(-5, 5, 2);
+        Assertions.assertThrowsExactly(IllegalStateException.class,
+                () -> linkedListTabulatedFunction.deletePoint(0));
+    }
+    @Test
+    void deletePointExceptionLeftBorder() {
+        Assertions.assertThrowsExactly(FunctionPointIndexOutOfBoundsException.class,
+                () -> linkedListTabulatedFunction.deletePoint(-1));
+    }
+
+    @Test
+    void deletePointExceptionRightBorder() {
+        Assertions.assertThrowsExactly(FunctionPointIndexOutOfBoundsException.class,
+                () -> linkedListTabulatedFunction.deletePoint(9));
+    }
+    @Test
+    void addPointExceptions() {
+        FunctionPoint functionPoint = Mockito.mock(FunctionPoint.class);
+        Mockito.when(functionPoint.getX()).thenReturn(-5.0);
+
+        Assertions.assertThrowsExactly(InappropriateFunctionPointException.class,
+                () -> linkedListTabulatedFunction.addPoint(functionPoint));
+    }
+}
