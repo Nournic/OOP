e9ba0e448cc67ad88396b2ef2f91da87eb9f117a
Nournic <ploxind@bk.ru>
2024-09-15 15:14:59 +0400

    Task 5 Experimental

--- LR/src/main/java/ru/ssau/tk/nour/oop/functions/TabulatedFunctions.java
+++ LR/src/main/java/ru/ssau/tk/nour/oop/functions/TabulatedFunctions.java
@@ -1,6 +1,8 @@
 package ru.ssau.tk.nour.oop.functions;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class TabulatedFunctions {
     public static TabulatedFunction tabulate(Function function, double leftX, double rightX, int pointsCount) throws IllegalArgumentException{
@@ -24,5 +26,27 @@ public abstract class TabulatedFunctions {
             tabulatedFunction = new ArrayTabulatedFunction(functionPoints);
 
         return tabulatedFunction;
+    }
+
+    public static void outputTabulatedFunction(TabulatedFunction function, OutputStream out) throws IOException {
+        int length = function.getPointsCount();
+
+        for(int i = 0; i < length; i++){
+            out.write(((Double)function.getPoint(i).getX()).byteValue());
+        }
+    }
+
+    public static TabulatedFunction inputTabulatedFunction(InputStream in) throws IOException, ClassNotFoundException {
+        ObjectInputStream objectInputStream = new ObjectInputStream(in);
+        List<FunctionPoint> list = new ArrayList<>();
+        while(objectInputStream.available() != 0){
+            list.add((FunctionPoint) objectInputStream.readObject());
+        }
+        FunctionPoint[] functionPoints = new FunctionPoint[list.size()];
+        for (int i = 0; i < functionPoints.length; i++) {
+            functionPoints[i] = list.get(i);
+        }
+
+        return new ArrayTabulatedFunction(functionPoints);
     }
 }
