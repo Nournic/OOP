753c2122c3da9e4b69bd05db1b849c134a8346f2
Nournic <ploxind@bk.ru>
2024-09-20 10:22:29 +0400

    Task 7

--- LR/src/main/java/ru/ssau/tk/nour/oop/functions/TabulatedFunctions.java
+++ LR/src/main/java/ru/ssau/tk/nour/oop/functions/TabulatedFunctions.java
@@ -30,22 +30,64 @@ public abstract class TabulatedFunctions {
 
     public static void outputTabulatedFunction(TabulatedFunction function, OutputStream out) throws IOException {
         int length = function.getPointsCount();
+        DataOutputStream dataOutputStream = new DataOutputStream(out);
 
         for(int i = 0; i < length; i++){
-            out.write(((Double)function.getPoint(i).getX()).byteValue());
+            dataOutputStream.writeDouble(function.getPointX(i));
+            dataOutputStream.writeDouble(function.getPointY(i));
         }
     }
 
-    public static TabulatedFunction inputTabulatedFunction(InputStream in) throws IOException, ClassNotFoundException {
-        ObjectInputStream objectInputStream = new ObjectInputStream(in);
+    public static TabulatedFunction inputTabulatedFunction(InputStream in) throws IOException {
+        DataInputStream dataInputStream = new DataInputStream(in);
         List<FunctionPoint> list = new ArrayList<>();
-        while(objectInputStream.available() != 0){
-            list.add((FunctionPoint) objectInputStream.readObject());
+
+        //Чтение данных в формате X Y и запись в список
+        while(dataInputStream.available() != 0){
+            double x = dataInputStream.readDouble();
+            double y = dataInputStream.readDouble();
+
+            list.add(new FunctionPoint(x,y));
         }
+
+        //Перевод списка в массив
         FunctionPoint[] functionPoints = new FunctionPoint[list.size()];
-        for (int i = 0; i < functionPoints.length; i++) {
+        for (int i = 0; i < list.size(); i++)
             functionPoints[i] = list.get(i);
+
+        return new ArrayTabulatedFunction(functionPoints);
+    }
+
+    public static void writeTabulatedFunction(TabulatedFunction function, Writer out) throws IOException{
+        int length = function.getPointsCount();
+
+        for(int i = 0; i < length; i++){
+            out.write(((Double)function.getPointX(i)).toString());
+            out.write(((Double)function.getPointY(i)).toString());
         }
+    }
+
+    public static TabulatedFunction readTabulatedFunction(Reader in) throws IOException{
+        List<Double> num_list = new ArrayList<>();
+        StreamTokenizer streamTokenizer = new StreamTokenizer(in);
+
+        int token = streamTokenizer.nextToken();
+        while (token != StreamTokenizer.TT_EOF) {
+            token = streamTokenizer.nextToken();
+            switch (token) {
+                case StreamTokenizer.TT_NUMBER:
+                    double num = streamTokenizer.nval;
+                    num_list.add(num);
+                    break;
+                case StreamTokenizer.TT_EOF:
+                    break;
+            }
+        }
+
+        //Перевод списка в массив
+        FunctionPoint[] functionPoints = new FunctionPoint[num_list.size()];
+        for (int i = 0; i < functionPoints.length; i+=2)
+            functionPoints[i] = new FunctionPoint(num_list.get(i),num_list.get(i+1));
 
         return new ArrayTabulatedFunction(functionPoints);
     }
