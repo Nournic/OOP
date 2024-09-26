257980a308a13fec8737ab891008d33debe47cf5
Плохин <ploxind@bk.ru>
2024-09-26 15:05:11 +0400

    fixes main

--- LR/src/main/java/ru/ssau/tk/nour/oop/Main.java
+++ LR/src/main/java/ru/ssau/tk/nour/oop/Main.java
@@ -8,23 +8,31 @@ public class Main {
         TabulatedFunction tabulatedFunction = new LinkedListTabulatedFunction(-10, 10, values);
 
         try {
+            System.out.println("Вывод значений функции:");
             double step = 20.0 / (values.length - 1);
+            System.out.println("Шаг: " + step);
             double current_step = -10.0 - step;
             for (; current_step <= 10 + 2 * step; current_step += step) {
-                System.out.println(tabulatedFunction.getFunctionValue(current_step));
+                System.out.println(Math.round(current_step) + ": " + tabulatedFunction.getFunctionValue(current_step));
             }
+            System.out.println();
 
-            System.out.println(tabulatedFunction.getFunctionValue(-9));
-            System.out.println(tabulatedFunction.getFunctionValue(-8.7));
-            System.out.println(tabulatedFunction.getFunctionValue(-10 + step));
+            System.out.println("Проверка методов:");
+            System.out.println("-9: " + tabulatedFunction.getFunctionValue(-9));
+            System.out.println("-8.7: " + tabulatedFunction.getFunctionValue(-8.7));
+            System.out.println(String.format("%d ",Math.round(-10.0+step)) + tabulatedFunction.getFunctionValue(-10 + step));
+
             FunctionPoint functionPoint = new FunctionPoint(-7, 0);
             tabulatedFunction.addPoint(functionPoint);
-            System.out.println(tabulatedFunction.getFunctionValue(-7));
+            System.out.println("-7: " + tabulatedFunction.getFunctionValue(-7));
+
             tabulatedFunction.setPointX(0, -12.5);
-            System.out.println(tabulatedFunction.getFunctionValue(-9));
+            System.out.println("-9: " + tabulatedFunction.getFunctionValue(-9));
+
             tabulatedFunction.deletePoint(0);
-            System.out.println(tabulatedFunction.getFunctionValue(-10 + step));
-            System.out.println(tabulatedFunction.getFunctionValue(-10));
+            System.out.println(String.format("%d ",Math.round(-10.0+step)) + tabulatedFunction.getFunctionValue(-10 + step));
+            System.out.println("-10: " + tabulatedFunction.getFunctionValue(-10));
+
         } catch (InappropriateFunctionPointException e) {
             System.out.println("Точка не лежит в области определения");
         }
