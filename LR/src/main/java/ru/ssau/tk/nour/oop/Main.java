6adec5a997b704612e919c26104400bdb19574d3
Nournic <ploxind@bk.ru>
2024-10-04 11:58:13 +0400

    Task 2

--- LR/src/main/java/ru/ssau/tk/nour/oop/Main.java
+++ LR/src/main/java/ru/ssau/tk/nour/oop/Main.java
@@ -1,27 +1,36 @@
 package ru.ssau.tk.nour.oop;
 
-import ru.ssau.tk.nour.oop.functions.Function;
+import ru.ssau.tk.nour.oop.functions.Functions;
 import ru.ssau.tk.nour.oop.functions.basic.Log;
 import ru.ssau.tk.nour.oop.functions.threads.Task;
 
 
 public class Main {
-    public static void main(String[] args) {
-
-    }
-
-    void nonThread(){
+    private static void nonThread(){
         Task task = new Task();
         task.setCountTasks(100);
-        Function function = new Log((int)(Math.random()*10)%10+1);
-        double leftBorder = Math.random()*100;
-        double rightBorder = Math.random()*100 + 100;
-        double step = Math.random();
+        for (int i = 0; i < task.getCountTasks(); i++) {
+            double base = (Math.random()*10)%10+1;
+            task.setFunction(new Log(base));
+            task.setLeftBorder(Math.random()*100);
+            task.setRightBorder(Math.random()*100 + 100);
+            task.setStep(Math.random());
 
-        task.setFunction(function).setLeftBorder(leftBorder)
-                .setRightBorder(rightBorder).setStep(step);
+            System.out.printf("%d. Source <%f> <%f> <%f>\n",
+                    i+1, task.getLeftBorder(), task.getRightBorder(), task.getStep());
 
-        System.out.printf("Source <%f> <%f> <%f>\n", leftBorder, rightBorder, step);
+            double result_integrate = Functions.integrate(
+                    task.getFunction(),task.getLeftBorder(),task.getRightBorder(),task.getStep());
+
+            System.out.printf("%d. Result <%f> <%f> <%f> <%f>\n",
+                    i+1, task.getLeftBorder(), task.getRightBorder(), task.getStep(), result_integrate);
+        }
 
     }
+
+    public static void main(String[] args) {
+        nonThread();
+    }
+
+
 }
